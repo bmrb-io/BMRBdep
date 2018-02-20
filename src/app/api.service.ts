cbdbd8564fbe8fc70bc9b53a991b47250629921f
Jon Wedell <wedell@bmrb.wisc.edu>
2018-02-20 14:50:38 -0600

    Update API service to cache entries.

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -21,15 +21,37 @@ export class ApiService {
 
   getEntry(entry_id: string, skip_cache: boolean = false): Observable<Entry> {
 
-    if ((entry_id === this.cached_entry.data_name) && (!skip_cache)) {
+    if ((entry_id === this.cached_entry.entry_id) && (!skip_cache)) {
+      console.log('Loaded entry from session memory.');
+      this.saveLocal();
       return of(this.cached_entry);
+    } else if ((entry_id === localStorage.getItem('entry_key')) && (!skip_cache)) {
+      this.loadLocal();
+      return of (this.cached_entry);
     } else {
       const entry_url = `https://webapi.bmrb.wisc.edu/v2/entry/${entry_id}`;
       return this.http.get(entry_url).map(json_data => {
          this.cached_entry = entryFromJSON(json_data[entry_id]);
+         console.log('Loaded entry from API.');
+         this.saveLocal();
          return this.cached_entry;
        });
     }
+  }
+
+  private saveLocal(): void {
+    localStorage.setItem('entry', JSON.stringify(this.cached_entry));
+    localStorage.setItem('entry_key', this.cached_entry.entry_id);
+    console.log('Saved entry to local storage.');
+  }
+
+  private loadLocal(): void {
+    this.cached_entry = entryFromJSON(JSON.parse(localStorage.getItem('entry')));
+    console.log('Loaded entry from local storage.');
+  }
+
+  getEntryID(): string {
+    return this.cached_entry.entry_id;
   }
 
   /* obsolete but kept for now for reference
