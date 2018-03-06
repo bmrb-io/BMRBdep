1ecaf12e472561249faa547f35cb499855c3ddfa
Jon Wedell <wedell@bmrb.wisc.edu>
2018-03-06 13:15:19 -0600

    Remove unused deepcopy.

--- src/app/nmrstar/saveframe.ts
+++ src/app/nmrstar/saveframe.ts
@@ -4,51 +4,6 @@ import { cleanValue } from './nmrstar';
 import { Schema } from './schema';
 import { SaveframeTag } from './tag';
 
-function deepCopy(obj) {
-  let copy;
-
-  // Handle the 3 simple types, and null or undefined
-  if (null === obj || 'object' !== typeof obj) {
-    return obj;
-  }
-
-  // Handle Date
-  if (obj instanceof Date) {
-    copy = new Date();
-    copy.setTime(obj.getTime());
-    return copy;
-  }
-
-  // Handle Array
-  if (obj instanceof Array) {
-    copy = [];
-    for (let i = 0, len = obj.length; i < len; i++) {
-      copy[i] = deepCopy(obj[i]);
-    }
-    return copy;
-  }
-
-  if (typeof(obj) === 'function') {
-    return obj;
-  }
-
-  // Handle Object
-  if (obj instanceof Object) {
-    copy = {};
-    for (const attr in obj) {
-      if (obj.hasOwnProperty(attr)) {
-        if (attr !== 'parent') {
-          copy[attr] = deepCopy(obj[attr]);
-        }
-      }
-    }
-    return copy;
-  }
-
-  throw new Error('Unable to copy obj! Its type isn\'t supported.');
-}
-
-
 export class Saveframe {
   name: string;
   category: string;
