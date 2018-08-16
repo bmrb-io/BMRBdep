53f322bb44b2716ec0bf6200f9e1d6de59497c19
Jon Wedell <wedell@bmrb.wisc.edu>
2018-08-16 15:25:07 -0500

    Listen for changes and scroll to the top of the page on route change.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
     console.log('InstantDep version .2');
+
+    // Scroll to the top of the page on route change
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0);
+    });
   }
 
 }
