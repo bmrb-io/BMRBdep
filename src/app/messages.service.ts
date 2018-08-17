0b620013069d93bfbff8b9edcae7c5f97b971cce
Jon Wedell <wedell@bmrb.wisc.edu>
2018-08-17 10:40:51 -0500

    Create new Message class to use for sending messages, update main component to show messages color-coded by type, change e-mail validation from build-into page to using standard messaging interface.

--- src/app/messages.service.ts
+++ src/app/messages.service.ts
@@ -1,6 +1,42 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 
+export enum MessageType {
+  SuccessMessage,
+  WarningMessage,
+  ErrorMessage,
+  NotificationMessage
+}
+
+export class Message {
+  messageBody: string;
+  messageType: MessageType;
+  messageTimeout: number;
+
+  constructor(messageBody: string,
+              messageType: MessageType = MessageType.SuccessMessage,
+              messageTimeout: number = 3000) {
+    this.messageBody = messageBody;
+    this.messageType = messageType;
+    this.messageTimeout = messageTimeout;
+  }
+
+  getClass() {
+    if (this.messageType === MessageType.SuccessMessage){
+      return 'SuccessMessage';
+    }
+    if (this.messageType === MessageType.WarningMessage){
+      return 'WarningMessage';
+    }
+    if (this.messageType === MessageType.ErrorMessage){
+      return 'ErrorMessage';
+    }
+    if (this.messageType === MessageType.NotificationMessage){
+      return 'NotificationMessage';
+    }
+  }
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,8 +46,8 @@ export class MessagesService {
 
   constructor() { }
 
-  sendMessage(message: string) {
-    this.subject.next({ text: message });
+  sendMessage(message: Message) {
+    this.subject.next(message);
   }
 
   clearMessage() {
