77a97134a5924f18cb1381b6e27db819687c25ac
Jon Wedell <wedell@bmrb.wisc.edu>
2018-08-22 11:40:17 -0500

    More improvements to message. Move all message subscription code to message component and strip it out of app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,18 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-import { Message, MessagesService } from './messages.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
 
-  subscription: Subscription;
-  message: Message;
-
-  constructor(private router: Router,
-              private messagesService: MessagesService) {
-
-    this.subscription = this.messagesService.getMessage().subscribe(message => {
-      this.message = message;
-    });
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+  constructor(private router: Router) {
   }
 
   ngOnInit() {
-    console.log('InstantDep version .2');
+    console.log('InstantDep version .3');
 
     // Scroll to the top of the page on route change
     this.router.events.subscribe((evt) => {
