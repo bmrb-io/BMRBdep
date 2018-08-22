75d152662612a07b64c085117abc0983022cdd34
Jon Wedell <wedell@bmrb.wisc.edu>
2018-08-22 11:01:43 -0500

    File uploader component folder wasn't added in previous commit.

---
+++ src/app/file-uploader/file-uploader.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {ApiService} from '../api.service';
+
+@Component({
+  selector: 'app-file-uploader',
+  templateUrl: './file-uploader.component.html',
+  styleUrls: ['./file-uploader.component.css']
+})
+export class FileUploaderComponent implements OnInit {
+
+  constructor(private api: ApiService) { }
+
+  ngOnInit() {
+  }
+
+  // At the drag drop area
+  // (drop)="onDropFile($event)"
+  onDropFile(event: DragEvent) {
+    event.preventDefault();
+    this.uploadFile(event.dataTransfer.files);
+  }
+
+  // At the drag drop area
+  // (dragover)="onDragOverFile($event)"
+  onDragOverFile(event) {
+    event.stopPropagation();
+    event.preventDefault();
+  }
+
+  // At the file input element
+  // (change)="selectFile($event)"
+  selectFile(event) {
+    this.uploadFile(event.target.files);
+  }
+
+  uploadFile(files: FileList) {
+    if (files.length === 0) {
+      console.log('No file selected!');
+      return;
+
+    }
+    for (let i = 0; i < files.length; i++) {
+      const file: File = files[i];
+      this.api.uploadFile(file);
+    }
+  }
+
+}
