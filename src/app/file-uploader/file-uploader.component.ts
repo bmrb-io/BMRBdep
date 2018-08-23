e04cd6880a5e81df9301da9f36db6b698b13b5ec
Jon Wedell <wedell@bmrb.wisc.edu>
2018-08-23 12:14:25 -0500

    Get file uploading working pretty well (though without state restore), including a nice progress bar during uploads.

--- src/app/file-uploader/file-uploader.component.ts
+++ src/app/file-uploader/file-uploader.component.ts
@@ -1,18 +1,15 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ApiService} from '../api.service';
 import {Saveframe} from '../nmrstar/saveframe';
+import {HttpEventType, HttpResponse} from '@angular/common/http';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
-@Component({
-  selector: 'app-file-uploader',
-  templateUrl: './file-uploader.component.html',
-  styleUrls: ['./file-uploader.component.css']
-})
-export class FileUploaderComponent implements OnInit {
-
-  @Input() saveframe: Saveframe;
-  public dropdownList;
-  public selectedItems;
-  public dropdownSettings = {
+class DataFile {
+  dropdownList;
+  selectedItems;
+  fileName;
+  percent;
+  dropdownSettings = {
     singleSelection: false,
     idField: 1,
     textField: 0,
@@ -22,50 +19,88 @@ export class FileUploaderComponent implements OnInit {
     enableCheckAll: false,
   };
 
+  constructor(fileName: string, dropdownList: {}, selectedItems: {} = []) {
+    this.fileName = fileName;
+    this.dropdownList = dropdownList;
+    this.selectedItems = selectedItems;
+    this.percent = 0;
+  }
+
+  updateDropdown(dropdownList) {
+    this.dropdownList = dropdownList;
+  }
+}
+
+@Component({
+  selector: 'app-file-uploader',
+  templateUrl: './file-uploader.component.html',
+  styleUrls: ['./file-uploader.component.css']
+})
+export class FileUploaderComponent implements OnInit {
+
+  @Input() saveframe: Saveframe;
+  dataFiles: Array<DataFile>;
+
   constructor(public api: ApiService) { }
 
   ngOnInit() {
-    this.dropdownList = this.saveframe.parent.schema.file_upload_types;
-    this.selectedItems = [];
+    this.dataFiles = [new DataFile(null, this.saveframe.parent.schema.file_upload_types)];
   }
 
-  onItemSelect (item: any) {
+  addFile() {
+    this.dataFiles.push(new DataFile(null, this.saveframe.parent.schema.file_upload_types));
+  }
+
+  onItemSelect (item: any, dataFile: DataFile) {
     console.log(item);
   }
-  onSelectAll (items: any) {
+  onSelectAll (items: any, dataFile: DataFile) {
     console.log(items);
   }
 
   // At the drag drop area
   // (drop)="onDropFile($event)"
-  onDropFile(event: DragEvent) {
+  onDropFile(event: DragEvent, dataFile: DataFile) {
     event.preventDefault();
-    this.uploadFile(event.dataTransfer.files);
+    this.uploadFile(event.dataTransfer.files, dataFile);
   }
 
   // At the drag drop area
   // (dragover)="onDragOverFile($event)"
-  onDragOverFile(event) {
+  onDragOverFile(event, dataFile: DataFile) {
     event.stopPropagation();
     event.preventDefault();
   }
 
   // At the file input element
   // (change)="selectFile($event)"
-  selectFile(event) {
-    this.uploadFile(event.target.files);
+  selectFile(event, dataFile) {
+    this.addFile();
+    this.uploadFile(event.target.files, dataFile);
   }
 
-  uploadFile(files: FileList) {
+  uploadFile(files: FileList, dataFile: DataFile) {
     if (files.length === 0) {
-      console.log('No file selected!');
+      console.error('No file selected!');
       return;
+    }
+    //this.api.uploadFile(files[0]);
 
-    }
-    for (let i = 0; i < files.length; i++) {
-      const file: File = files[i];
-      this.api.uploadFile(file);
-    }
+    dataFile.fileName = files[0].name;
+    this.api.uploadFile(files[0])
+      .subscribe(
+        event => {
+          if (event.type === HttpEventType.UploadProgress) {
+            dataFile.percent = Math.round(100 * event.loaded / event.total);
+          } else if (event instanceof HttpResponse) {
+            dataFile.percent = 100;
+            dataFile.fileName = event.body['filename'];
+          }
+        },
+        error => {
+          this.api.handleError(error);
+        }
+      );
   }
 
 }
