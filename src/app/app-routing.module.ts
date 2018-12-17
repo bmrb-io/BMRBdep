9ae2be0d94f757390afd69e3e0a7729e65dcff27
Jon Wedell <wedell@bmrb.wisc.edu>
2018-12-17 15:58:21 -0600

    Fix auto-scrolling to top of page on route change.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})
   ],
   exports: [
     RouterModule
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {Router, NavigationEnd} from '@angular/router';
 import {Entry} from './nmrstar/entry';
 
 @Component({
@@ -12,20 +11,12 @@ export class AppComponent implements OnInit {
   entry: Entry;
   sidenav_open: boolean;
 
-  constructor(private router: Router) {
+  constructor() {
     this.sidenav_open = false;
   }
 
   ngOnInit() {
-    console.log('InstantDep version .3');
-
-    // Scroll to the top of the page on route change
-    this.router.events.subscribe((evt) => {
-      if (!(evt instanceof NavigationEnd)) {
-        return;
-      }
-      window.scrollTo(0, 0);
-    });
+    console.log('InstantDep version .4');
   }
 
 }
