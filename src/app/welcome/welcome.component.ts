a3f9030b9813cce648231ffcdaae76b1d826bc7a
Jon Wedell <jonwedell@gmail.com>
2019-01-28 12:11:40 -0600

    Totally re-do "new deposition" page to use Angular Material. Form validation is now completely built-in and error messages are more instant and helpful. Much cleaner look in general.
    Pending verification, should close #62 and #64.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,6 +1,7 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Router} from '@angular/router';
 import {ApiService} from '../api.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-welcome',
@@ -8,57 +9,65 @@ import {ApiService} from '../api.service';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
-  authorEmail: string;
-  depositionNickname: string;
-  authorORCID: string;
-  bootstrapID: string;
-  sessionType: string;
-  error: string;
-  validResumeID: 'valid' | 'adit' | 'invalid';
   @ViewChild('inputFile') fileUploadElement: ElementRef;
 
   constructor(private router: Router, private api: ApiService) {
-    this.authorEmail = '';
-    this.depositionNickname = '';
-    this.authorORCID = '';
-    this.bootstrapID = null;
-    this.error = '';
-    this.sessionType = 'new';
+  }
+
+  sessionType = new FormControl('new',);
+  authorEmail = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  depositionNickname = new FormControl('', [Validators.required]);
+  authorORCID = new FormControl('', [Validators.pattern(/^\d{4}-\d{4}-\d{4}-(\d{3}X|\d{4})$/)]);
+  bootstrapID = new FormControl('', [Validators.required, Validators.pattern(/^[0-9]+$/)]);
+
+  createDepositionForm: FormGroup = new FormGroup({
+    sessionType: this.sessionType,
+    authorEmail: this.authorEmail,
+    depositionNickname: this.depositionNickname,
+    authorORCID: this.authorORCID,
+  });
+
+  getEmailErrorMessage(emailForm: FormControl) {
+    return emailForm.hasError('required') ? 'You must enter your email address.' :
+      emailForm.hasError('email') ? 'Not a valid email address.' : '';
+  }
+
+  getBootstrapErrorMessage(bootstrapForm: FormControl) {
+    return bootstrapForm.hasError('required') ? 'You must enter the ID of an existing entry.' :
+      bootstrapForm.hasError('pattern') ? 'Not a valid BMRB ID.' : '';
   }
 
   ngOnInit() {
   }
 
-  validate(sessionID): void {
-    const regexp = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-    const aditRegexp = /[0-9]+-[0-9]+-[0-9]+(\.[\S]+)+\.[0-9]+\.[0-9]+/i;
-    if (regexp.test(sessionID)) {
-      this.validResumeID = 'valid';
-    } else if (aditRegexp.test(sessionID)) {
-      this.validResumeID = 'adit';
-    } else {
-      this.validResumeID = 'invalid';
-    }
+  // This is needed for angular to detect the file upload
+  fileChangeEvent() {
   }
 
-  // This is needed for angular to detect the file upload
-  fileChangeEvent() {}
+  new(f: FormGroup) {
 
-  new() {
-    let bootstrapID = this.bootstrapID;
-    let fileElement = null;
-    if (this.sessionType === 'file') {
-      fileElement = this.fileUploadElement.nativeElement.files[0];
-    } else if (this.sessionType === 'bmrb_id') {
-      bootstrapID = this.bootstrapID;
-    } else {
-
+    if (!f.valid) {
+      return;
     }
-    this.api.newDeposition(this.authorEmail, this.depositionNickname, this.authorORCID, fileElement, bootstrapID).subscribe(
+
+    if (f.value.sessionType === 'bmrb_id' && !this.bootstrapID.value) {
+      return;
+    }
+
+    let fileElement = null;
+    if (f.value.sessionType === 'file') {
+      fileElement = this.fileUploadElement.nativeElement.files[0];
+    }
+
+    this.api.newDeposition(f.value.authorEmail, f.value.depositionNickname, f.value.authorORCID, fileElement, this.bootstrapID.value).subscribe(
       response => {
         if (response) {
           this.router.navigate(['/entry/', response['deposition_id'], 'saveframe', 'deposited_data_files', 'category']);
         }
       });
+
   }
 }
