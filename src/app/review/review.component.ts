a3a0d1721fe1289f187702823aae3d5a8b194697
Jon Wedell <jon@jonwedell.com>
2019-01-31 13:38:16 -0600

    Update review component to go back to previous page rather than to the deposit files section.

--- src/app/review/review.component.ts
+++ src/app/review/review.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ApiService} from '../api.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Message, MessagesService, MessageType} from '../messages.service';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-review',
@@ -13,7 +14,8 @@ export class ReviewComponent implements OnInit {
   constructor(public api: ApiService,
               private route: ActivatedRoute,
               private messagesService: MessagesService,
-              private router: Router) {
+              private router: Router,
+              private location: Location) {
   }
 
   ngOnInit() {
@@ -22,6 +24,10 @@ export class ReviewComponent implements OnInit {
     this.route.params.subscribe(function (params) {
       parent.api.getEntry(params['entry']).subscribe();
     });
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
   submitEntry(): void {
