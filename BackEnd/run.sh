f972b356414dcc301410f93670cf53034c83a436
Jon Wedell <jonwedell@gmail.com>
2019-02-12 15:14:59 -0600

    Move all the front end code to its own directory. Add most of the backend code, and a newly implemented python3 compatible verify_email module. Work towards total containerization.

---
+++ BackEnd/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+source ${DIR}/env/bin/activate
+export FLASK_APP=bmrbdep.py
+export FLASK_DEBUG=1
+flask run --host=0.0.0.0 --port 9000
