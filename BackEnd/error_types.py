9f926438838e6e608cbaf6db6c474f9307a9aa76
Jon Wedell <jonwedell@gmail.com>
2019-02-12 15:14:59 -0600

    Move all the front end code to its own directory. Add most of the backend code, and a newly implemented python3 compatible verify_email module. Work towards total containerization.

---
+++ BackEnd/error_types.py
@@ -0,0 +1,34 @@
+class ServerError(Exception):
+    """ Something is wrong with the server. """
+    status_code = 500
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        """ Converts the payload to a dictionary."""
+        rv = dict(self.payload or ())
+        rv['error'] = self.message
+        return rv
+
+
+class RequestError(Exception):
+    """ Something is wrong with the request. """
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        """ Converts the payload to a dictionary."""
+        rv = dict(self.payload or ())
+        rv['error'] = self.message
+        return rv
