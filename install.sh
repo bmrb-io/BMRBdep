bd4bbf550d8c4db79e2a5457b1757b234ca6b7dd
Jon Wedell <jonwedell@gmail.com>
2019-02-15 10:17:05 -0600

    Completely tested and verified one-step install script (which is automatically called from run_local and build_docker scripts. It now:
    *) Sets up the node environment
    *) Sets up the python virtualenv
    *) Copies the default configuration file to the config file location
    *) Builds the most recent version of the schema file (needed to run)
    
    And logs all of those steps to a local file.

--- install.sh
+++ install.sh
@@ -4,22 +4,26 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [[ ! -d "${SCRIPT_DIR}/FrontEnd/node_env" ]]
 then
-  echo "node environment was not yet set up. Setting up now... (This only needs to happen once.)"
-  ${SCRIPT_DIR}/FrontEnd/setup.sh
+  echo "node environment was not yet set up. Setting up now... (This only needs to happen once.)" | tee -a ${SCRIPT_DIR}/installation.log
+  ${SCRIPT_DIR}/FrontEnd/setup.sh 2>&1 | tee -a ${SCRIPT_DIR}/installation.log
 fi
 
 if [[ ! -d "${SCRIPT_DIR}/BackEnd/env" ]]
 then
-  echo "python environment was not yet set up. Setting up now... (This only needs to happen once.)"
-  ${SCRIPT_DIR}/BackEnd/setup.sh
+  echo "python environment was not yet set up. Setting up now... (This only needs to happen once.)" | tee -a ${SCRIPT_DIR}/installation.log
+  ${SCRIPT_DIR}/BackEnd/setup.sh 2>&1 | tee -a ${SCRIPT_DIR}/installation.log
 fi
 
+if [[ ! -f "${SCRIPT_DIR}/BackEnd/app/configuration.json" ]]
+then
+  echo "No configuration file found. Copying example configuration file (which will work for local testing) to ${SCRIPT_DIR}/BackEnd/app/configuration.json. You should edit this file before deployment." | tee -a ${SCRIPT_DIR}/installation.log
+  cp ${SCRIPT_DIR}/BackEnd/app/example_config.json ${SCRIPT_DIR}/BackEnd/app/configuration.json
+fi
 
 if [[ ! -f "${SCRIPT_DIR}/BackEnd/app/schema_data/3.2.1.21.json.zlib" ]]
 then
-    echo "Schemas not found. Generating local cache of schema versions. This may take ~10 minutes. (This only needs to happen once.)"
+    echo "Schemas not found. Generating local cache of schema versions. This may take ~10 minutes. (This only needs to happen once.)" | tee -a ${SCRIPT_DIR}/installation.log
     source ${SCRIPT_DIR}/BackEnd/env/bin/activate
-    ${SCRIPT_DIR}/BackEnd/app/schema_loader.py
+    ${SCRIPT_DIR}/BackEnd/app/schema_loader.py 2>&1 | tee -a ${SCRIPT_DIR}/installation.log
     deactivate
 fi
-
