ca3468421d5c0cfbfd7a087bb3c288fd521a390e
Jon Wedell <jonwedell@gmail.com>
2019-02-15 08:46:41 -0600

    Fix path issue when running backend setup script from different directory.

--- BackEnd/setup.sh
+++ BackEnd/setup.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 python3 -m venv ${DIR}/env
 source ${DIR}/env/bin/activate
-pip3 install -r app/requirements.txt
+pip3 install -r ${DIR}/app/requirements.txt
