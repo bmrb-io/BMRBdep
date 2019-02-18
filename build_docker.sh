c298b56a561dc1665b9c27fc66c9501feb36e94f
Jon Wedell <jonwedell@gmail.com>
2019-02-18 11:31:37 -0600

    Updates to the docker configurations now that the uwsgi plugin is working.

--- build_docker.sh
+++ build_docker.sh
@@ -19,19 +19,8 @@ if [[ $# -eq 0 ]]
   then
     sudo docker build -t bmrbdep .
     echo "Running in development mode."
+    sudo docker run -d --name bmrbdep  -p 9000:9000 --restart=always -v /tmp/depositions:/opt/wsgi/depositions bmrbdep
   else
     sudo docker build --build-arg configfile=$1 -t bmrbdep .
     echo "Running with configuration file: $1"
 fi
-
-
-sudo docker run -d --name bmrbdep  -p 9000:9000 --restart=always -v /zfs/git/depositions:/opt/wsgi/depositions bmrbdep
-
-#docker tag bmrbdep pike.bmrb.wisc.edu:5000/bmrbdep.001
-#docker push pike.bmrb.wisc.edu:5000/bmrbdep.001
-
-
-#docker pull pike.bmrb.wisc.edu:5000/bmrbdep.001
-#sudo docker stop bmrbdep
-#sudo docker rm bmrbdep
-#docker run -d --name bmrbdep -p 9000:9000 --restart=always -v /websites/bmrbdep/depositions:/opt/wsgi/depositions pike.bmrb.wisc.edu:5000/bmrbdep.001
