24e0b7456d175709f6f7348ac1309e121403a92b
Jon Wedell <jon@jonwedell.com>
2019-02-19 13:09:25 -0600

    Clean up the ApiService variable names and indentation.

--- FrontEnd/src/app/api.service.ts
+++ FrontEnd/src/app/api.service.ts
@@ -8,234 +8,234 @@ import {Message, MessagesService, MessageType} from './messages.service';
 import {Router} from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ApiService {
 
-  cachedEntry: Entry;
-  activeSaveRequest: Subscription;
+    cachedEntry: Entry;
+    activeSaveRequest: Subscription;
 
-  JSONOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
-  };
-
-  constructor(private http: HttpClient,
-              private messagesService: MessagesService,
-              private router: Router) {
-    this.cachedEntry = new Entry('');
-  }
-
-  clearDeposition(): void {
-      localStorage.removeItem('entry_key');
-      localStorage.removeItem('entry');
-      localStorage.removeItem('schema');
-      this.cachedEntry = new Entry('');
-  }
-
-  // file from event.target.files[0]
-  uploadFile(file: File): Observable<HttpEvent<any>> {
-
-    const apiEndPoint = `${environment.serverURL}/${this.getEntryID()}/file`;
-
-    const formData = new FormData();
-    formData.append('file', file);
-
-    const options = {
-      params: new HttpParams(),
-      reportProgress: true,
-    };
-
-    const req = new HttpRequest('POST', apiEndPoint, formData, options);
-    return this.http.request(req);
-  }
-
-  deleteFile(fileName: string): Observable<boolean> {
-    const apiEndPoint = `${environment.serverURL}/${this.getEntryID()}/file/${fileName}`;
-    this.http.delete(apiEndPoint).subscribe(
-      () => {
-        this.messagesService.sendMessage(new Message('File deleted.'));
-        return of(true);
-      },
-      () => {
-        this.messagesService.sendMessage(new Message('Failed to delete file.',
-          MessageType.ErrorMessage, 15000));
-        return of(false);
-      }
-    );
-    return of(false);
-  }
-
-  getEntry(entry_id: string, skip_cache: boolean = false): Observable<Entry> {
-    // If all we did was reroute, we still have the entry
-    if ((entry_id === this.cachedEntry.entryID) && (!skip_cache)) {
-      this.cachedEntry['source'] = 'session memory';
-    // The page is being reloaded, but we can get the entry from the browser cache
-    } else if ((entry_id === localStorage.getItem('entry_key')) && (!skip_cache)) {
-
-      // Make sure both the entry and schema are saved locally - if not, getEntry() but force load from server
-      const raw_json = JSON.parse(localStorage.getItem('entry'));
-      if (raw_json === null) {
-        return this.getEntry(entry_id, true);
-      }
-      raw_json['schema'] = JSON.parse(localStorage.getItem('schema'));
-      if (raw_json['schema'] === null) {
-        return this.getEntry(entry_id, true);
-      }
-
-      this.cachedEntry = entryFromJSON(raw_json);
-      this.cachedEntry['source'] = 'browser cache';
-    // We either don't have the entry or have a different one, so fetch from the API
-    } else {
-      const entry_url = `${environment.serverURL}/${entry_id}`;
-      return this.http.get(entry_url).pipe(
-        map(json_data => {
-          this.cachedEntry = entryFromJSON(json_data);
-          this.cachedEntry['source'] = 'API server';
-          this.saveEntry(true);
-          return this.cachedEntry;
-        }),
-        catchError(error => {
-          if (!environment.debug) {
-            this.messagesService.sendMessage(new Message('Invalid entry ID. Returning to main page in 10 seconds.',
-              MessageType.ErrorMessage, 10000));
-            setTimeout(() => {
-              this.router.navigate(['/']);
-            }, 10000);
-            return of(new Entry(entry_id));
-          } else {
-            throw error;
-          }
+    JSONOptions = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json'
         })
-      );
-    }
-
-    return of(this.cachedEntry);
-  }
-
-  saveEntry(initial_save: boolean = false, skipMessage: boolean = true): void {
-
-    // If the previous save action is still in progress, cancel it
-    if (this.activeSaveRequest) {
-      this.activeSaveRequest.unsubscribe();
-    }
-
-    if (initial_save) {
-      localStorage.setItem('schema', JSON.stringify(this.cachedEntry.schema));
-    }
-    localStorage.setItem('entry', JSON.stringify(this.cachedEntry));
-    localStorage.setItem('entry_key', this.cachedEntry.entryID);
-
-    // Save to remote server if we haven't just loaded the entry
-    if (!initial_save) {
-      const entry_url = `${environment.serverURL}/${this.cachedEntry.entryID}`;
-      const parent = this;
-      this.activeSaveRequest = this.http.put(entry_url, JSON.stringify(this.cachedEntry), this.JSONOptions).subscribe(
-        () => {
-          if (!skipMessage) {
-            this.messagesService.sendMessage(new Message('Changes saved.'));
-          }
-          parent.activeSaveRequest = null;
-        },
-        err => this.handleError(err)
-      );
-    }
-  }
-
-  newDeposition(authorEmail: string,
-                depositionNickname: string,
-                orcid: string = null,
-                file: File = null,
-                bootstrapID: string = null): Observable<any> {
-    const apiEndPoint = `${environment.serverURL}/new`;
-    this.messagesService.sendMessage(new Message('Creating deposition...',
-      MessageType.NotificationMessage, 0));
-
-    const body = new FormData();
-    body.append('email', authorEmail);
-    body.append('deposition_nickname', depositionNickname);
-    if (orcid) {
-      body.append('orcid', orcid);
-    }
-    if (file) {
-      body.append('nmrstar_file', file);
-    }
-    if (bootstrapID) {
-      body.append('bootstrapID', bootstrapID);
-    }
-
-    const options = {
-      params: new HttpParams(),
-      reportProgress: true,
     };
 
-    return this.http.post(apiEndPoint, body, options)
-      .pipe(
-        map(json_data => {
-          this.messagesService.clearMessage();
-          return json_data;
-        }),
-        // Convert the error into something we can handle
-        catchError((error: HttpErrorResponse) => this.handleError(error))
-      );
-  }
-
-  submitEntry(): Observable<any> {
-
-    if (!this.cachedEntry.valid) {
-      this.messagesService.sendMessage(new Message('Can not submit deposition: it is still incomplete!',
-        MessageType.WarningMessage, 15000));
-      return;
+    constructor(private http: HttpClient,
+                private messagesService: MessagesService,
+                private router: Router) {
+        this.cachedEntry = new Entry('');
     }
 
-    const apiEndPoint = `${environment.serverURL}/${this.getEntryID()}/deposit`;
+    clearDeposition(): void {
+        localStorage.removeItem('entry_key');
+        localStorage.removeItem('entry');
+        localStorage.removeItem('schema');
+        this.cachedEntry = new Entry('');
+    }
 
-    this.messagesService.sendMessage(new Message('Submitting deposition...',
-      MessageType.NotificationMessage, 0));
-    return this.http.post(apiEndPoint, null)
-      .pipe(
-        map(json_data => {
-          this.messagesService.clearMessage();
-          return json_data;
-        }),
-        // Convert the error into something we can handle
-        catchError((error: HttpErrorResponse) => this.handleError(error))
-      );
-  }
+    // file from event.target.files[0]
+    uploadFile(file: File): Observable<HttpEvent<any>> {
 
-  resendValidationEmail(): Observable<any> {
+        const apiEndPoint = `${environment.serverURL}/${this.getEntryID()}/file`;
 
-    const apiEndPoint = `${environment.serverURL}/${this.getEntryID()}/resend-validation-email`;
+        const formData = new FormData();
+        formData.append('file', file);
 
-    this.messagesService.sendMessage(new Message('Requesting new validation e-mail...',
-        MessageType.NotificationMessage, 0));
-    return this.http.get(apiEndPoint)
-        .pipe(
-            map(json_data => {
-              this.messagesService.clearMessage();
-              return json_data;
-            }),
-            // Convert the error into something we can handle
-            catchError((error: HttpErrorResponse) => this.handleError(error))
+        const options = {
+            params: new HttpParams(),
+            reportProgress: true,
+        };
+
+        const req = new HttpRequest('POST', apiEndPoint, formData, options);
+        return this.http.request(req);
+    }
+
+    deleteFile(fileName: string): Observable<boolean> {
+        const apiEndPoint = `${environment.serverURL}/${this.getEntryID()}/file/${fileName}`;
+        this.http.delete(apiEndPoint).subscribe(
+            () => {
+                this.messagesService.sendMessage(new Message('File deleted.'));
+                return of(true);
+            },
+            () => {
+                this.messagesService.sendMessage(new Message('Failed to delete file.',
+                    MessageType.ErrorMessage, 15000));
+                return of(false);
+            }
         );
-  }
-
-  getEntryID(): string {
-    return this.cachedEntry.entryID;
-  }
-
-  handleError(error: HttpErrorResponse) {
-    if (error.status === 400) {
-      this.messagesService.sendMessage(new Message(error.error.error,
-        MessageType.WarningMessage, 15000));
-    } else {
-      this.messagesService.sendMessage(new Message('A network or server exception occurred.', MessageType.ErrorMessage, 15000));
-      console.error('An unhandled server error code occurred:', error);
+        return of(false);
     }
 
-    return of(null);
-  }
+    getEntry(entryID: string, skipCache: boolean = false): Observable<Entry> {
+        // If all we did was reroute, we still have the entry
+        if ((entryID === this.cachedEntry.entryID) && (!skipCache)) {
+            this.cachedEntry['source'] = 'session memory';
+            // The page is being reloaded, but we can get the entry from the browser cache
+        } else if ((entryID === localStorage.getItem('entry_key')) && (!skipCache)) {
+
+            // Make sure both the entry and schema are saved locally - if not, getEntry() but force load from server
+            const rawJSON = JSON.parse(localStorage.getItem('entry'));
+            if (rawJSON === null) {
+                return this.getEntry(entryID, true);
+            }
+            rawJSON['schema'] = JSON.parse(localStorage.getItem('schema'));
+            if (rawJSON['schema'] === null) {
+                return this.getEntry(entryID, true);
+            }
+
+            this.cachedEntry = entryFromJSON(rawJSON);
+            this.cachedEntry['source'] = 'browser cache';
+            // We either don't have the entry or have a different one, so fetch from the API
+        } else {
+            const entryURL = `${environment.serverURL}/${entryID}`;
+            return this.http.get(entryURL).pipe(
+                map(jsonData => {
+                    this.cachedEntry = entryFromJSON(jsonData);
+                    this.cachedEntry['source'] = 'API server';
+                    this.saveEntry(true);
+                    return this.cachedEntry;
+                }),
+                catchError(error => {
+                    if (!environment.debug) {
+                        this.messagesService.sendMessage(new Message('Invalid entry ID. Returning to main page in 10 seconds.',
+                            MessageType.ErrorMessage, 10000));
+                        setTimeout(() => {
+                            this.router.navigate(['/']);
+                        }, 10000);
+                        return of(new Entry(entryID));
+                    } else {
+                        throw error;
+                    }
+                })
+            );
+        }
+
+        return of(this.cachedEntry);
+    }
+
+    saveEntry(initialSave: boolean = false, skipMessage: boolean = true): void {
+
+        // If the previous save action is still in progress, cancel it
+        if (this.activeSaveRequest) {
+            this.activeSaveRequest.unsubscribe();
+        }
+
+        if (initialSave) {
+            localStorage.setItem('schema', JSON.stringify(this.cachedEntry.schema));
+        }
+        localStorage.setItem('entry', JSON.stringify(this.cachedEntry));
+        localStorage.setItem('entry_key', this.cachedEntry.entryID);
+
+        // Save to remote server if we haven't just loaded the entry
+        if (!initialSave) {
+            const entryURL = `${environment.serverURL}/${this.cachedEntry.entryID}`;
+            const parent = this;
+            this.activeSaveRequest = this.http.put(entryURL, JSON.stringify(this.cachedEntry), this.JSONOptions).subscribe(
+                () => {
+                    if (!skipMessage) {
+                        this.messagesService.sendMessage(new Message('Changes saved.'));
+                    }
+                    parent.activeSaveRequest = null;
+                },
+                err => this.handleError(err)
+            );
+        }
+    }
+
+    newDeposition(authorEmail: string,
+                  depositionNickname: string,
+                  orcid: string = null,
+                  file: File = null,
+                  bootstrapID: string = null): Observable<any> {
+        const apiEndPoint = `${environment.serverURL}/new`;
+        this.messagesService.sendMessage(new Message('Creating deposition...',
+            MessageType.NotificationMessage, 0));
+
+        const body = new FormData();
+        body.append('email', authorEmail);
+        body.append('deposition_nickname', depositionNickname);
+        if (orcid) {
+            body.append('orcid', orcid);
+        }
+        if (file) {
+            body.append('nmrstar_file', file);
+        }
+        if (bootstrapID) {
+            body.append('bootstrapID', bootstrapID);
+        }
+
+        const options = {
+            params: new HttpParams(),
+            reportProgress: true,
+        };
+
+        return this.http.post(apiEndPoint, body, options)
+            .pipe(
+                map(jsonData => {
+                    this.messagesService.clearMessage();
+                    return jsonData;
+                }),
+                // Convert the error into something we can handle
+                catchError((error: HttpErrorResponse) => this.handleError(error))
+            );
+    }
+
+    submitEntry(): Observable<any> {
+
+        if (!this.cachedEntry.valid) {
+            this.messagesService.sendMessage(new Message('Can not submit deposition: it is still incomplete!',
+                MessageType.WarningMessage, 15000));
+            return;
+        }
+
+        const apiEndPoint = `${environment.serverURL}/${this.getEntryID()}/deposit`;
+
+        this.messagesService.sendMessage(new Message('Submitting deposition...',
+            MessageType.NotificationMessage, 0));
+        return this.http.post(apiEndPoint, null)
+            .pipe(
+                map(jsonData => {
+                    this.messagesService.clearMessage();
+                    return jsonData;
+                }),
+                // Convert the error into something we can handle
+                catchError((error: HttpErrorResponse) => this.handleError(error))
+            );
+    }
+
+    resendValidationEmail(): Observable<any> {
+
+        const apiEndPoint = `${environment.serverURL}/${this.getEntryID()}/resend-validation-email`;
+
+        this.messagesService.sendMessage(new Message('Requesting new validation e-mail...',
+            MessageType.NotificationMessage, 0));
+        return this.http.get(apiEndPoint)
+            .pipe(
+                map(jsonData => {
+                    this.messagesService.clearMessage();
+                    return jsonData;
+                }),
+                // Convert the error into something we can handle
+                catchError((error: HttpErrorResponse) => this.handleError(error))
+            );
+    }
+
+    getEntryID(): string {
+        return this.cachedEntry.entryID;
+    }
+
+    handleError(error: HttpErrorResponse) {
+        if (error.status === 400) {
+            this.messagesService.sendMessage(new Message(error.error.error,
+                MessageType.WarningMessage, 15000));
+        } else {
+            this.messagesService.sendMessage(new Message('A network or server exception occurred.', MessageType.ErrorMessage, 15000));
+            console.error('An unhandled server error code occurred:', error);
+        }
+
+        return of(null);
+    }
 
 
 }
