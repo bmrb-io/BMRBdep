975f61b9babac02ad87a87c3b671c250ddd7c390
Jon Wedell <jon@jonwedell.com>
2019-04-04 14:12:23 -0500

    Revert accidentally applied merge.

--- FrontEnd/src/app/entry/entry.component.ts
+++ FrontEnd/src/app/entry/entry.component.ts
@@ -1,20 +1,26 @@
 import {ApiService} from '../api.service';
 import {Entry} from '../nmrstar/entry';
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-entry',
   templateUrl: './entry.component.html',
   styleUrls: ['./entry.component.css']
 })
-export class EntryComponent implements OnInit {
+export class EntryComponent implements OnInit, OnDestroy {
   entry: Entry;
+  subscription$: Subscription;
 
   constructor(private api: ApiService) {
   }
 
   ngOnInit() {
-    this.api.entrySubject.subscribe(entry => this.entry = entry);
+    this.subscription$ = this.api.entrySubject.subscribe(entry => this.entry = entry);
+  }
+
+  ngOnDestroy() {
+    this.subscription$.unsubscribe();
   }
 
 }
