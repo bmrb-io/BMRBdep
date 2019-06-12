1947b4d7c355e41d0b0d821ca9019644196166b9
Jon Wedell <jonwedell@gmail.com>
2019-06-12 12:20:21 +0200

    Add a new ContactUsComponent to allow users to send a message to support. Update the API to support sending a request to the ZenDesk API.
    Do all the other work to make this feature production ready.

--- FrontEnd/src/environments/environment.prod.ts
+++ FrontEnd/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   debug: false,
   serverURL: '/deposition',
-  contactEmail: 'bmrbhelp@bmrb.wisc.edu'
+  contactEmail: 'support@bmrb.zendesk.com',
+  supportURL: 'https://bmrb.zendesk.com/api/v2/requests.json'
 };
