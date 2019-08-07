5c6c47537631b68430927f700d2421bcd688e041
Jon Wedell <jon@jonwedell.com>
2019-08-07 12:54:34 -0500

    Add release scripts.

---
+++ development_release.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "Getting newest schema."
+source ${SCRIPT_DIR}/BackEnd/schema_venv/bin/activate
+if ! ${SCRIPT_DIR}/BackEnd/app/schema_loader.py; then
+  echo "Schema loader failed, quitting."
+  exit
+fi
+deactivate
+
+echo "Compiling angular."
+source ${SCRIPT_DIR}/FrontEnd/node_env/bin/activate
+cd ${SCRIPT_DIR}/FrontEnd
+if ! npm run build.prod; then
+  echo "Angular build failed, quitting."
+  exit
+fi
+cd dist
+tar -czvf ../release.tgz * .htaccess
+scp ../release.tgz wedell@manta:/tmp/
+cd ..;
+rm -rfv release.tgz
+cd ${SCRIPT_DIR}
+
+echo "Building docker instance..."
+if ! ${SCRIPT_DIR}/build_docker.sh production.conf; then
+  echo "Building docker instance failed, quitting."
+  exit
+fi
+
+echo "Deploying Angular..."
+echo "cd /websites/bmrbdep/html; rm -fv /websites/bmrbdep/html/*; tar -xzvf /tmp/release.tgz" | ssh wedell@manta
+
+echo "Deploying docker instance..."
+sudo docker tag bmrbdep pike.bmrb.wisc.edu:5000/bmrbdep
+sudo docker push pike.bmrb.wisc.edu:5000/bmrbdep
+
+echo "sudo docker pull pike.bmrb.wisc.edu:5000/bmrbdep; sudo docker stop bmrbdep; sudo docker rm bmrbdep; sudo docker run -d --name bmrbdep -p 9000:9000 --restart=always -v /websites/bmrbdep/configuration.json:/opt/wsgi/configuration.json -v /bmrbdep/depositions:/opt/wsgi/depositions pike.bmrb.wisc.edu:5000/bmrbdep" | ssh manta
+
+#
+# sudo docker pull pike.bmrb.wisc.edu:5000/bmrbdep; sudo docker stop bmrbdep; sudo docker rm bmrbdep;
+# sudo docker run -d --name bmrbdep -p 9000:9000 --restart=always -v /depositions:/opt/wsgi/depositions -v /websites/bmrbdep/configuration.json:/opt/wsgi/configuration.json pike.bmrb.wisc.edu:5000/bmrbdep
