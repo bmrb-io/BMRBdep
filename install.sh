fdf0f6debf8a94d509f333ef4d6c189f4c84ddad
Jon Wedell <jonwedell@gmail.com>
2019-08-07 14:56:16 -0500

    Make all the shell scripts more robust in case of a space or hyphen in a filename, and related things.

--- install.sh
+++ install.sh
@@ -1,53 +1,48 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-if [[ ! -d "${SCRIPT_DIR}/BackEnd/env" ]] || [[ "$1" = "-force" ]]
-then
-  echo "python environment was not yet set up. Setting up now... (This only needs to happen once.)" | tee -a ${SCRIPT_DIR}/installation.log
-  python3 -m venv ${SCRIPT_DIR}/BackEnd/env
-  source ${SCRIPT_DIR}/BackEnd/env/bin/activate
-  pip3 install --upgrade pip | tee -a ${SCRIPT_DIR}/installation.log
-  pip3 install -r ${SCRIPT_DIR}/BackEnd/app/requirements.txt | tee -a ${SCRIPT_DIR}/installation.log
+if [[ ! -d "${SCRIPT_DIR}/BackEnd/env" ]] || [[ "$1" == "-force" ]]; then
+  echo "python environment was not yet set up. Setting up now... (This only needs to happen once.)" | tee -a "${SCRIPT_DIR}"/installation.log
+  python3 -m venv "${SCRIPT_DIR}"/BackEnd/env
+  source "${SCRIPT_DIR}"/BackEnd/env/bin/activate
+  pip3 install --upgrade pip | tee -a "${SCRIPT_DIR}"/installation.log
+  pip3 install -r "${SCRIPT_DIR}"/BackEnd/app/requirements.txt | tee -a "${SCRIPT_DIR}"/installation.log
   deactivate
 fi
 
-if [[ ! -d "${SCRIPT_DIR}/FrontEnd/node_env" ]] || [[ "$1" = "-force" ]]
-then
-  echo "node environment was not yet set up. Setting up now... (This only needs to happen once.)" | tee -a ${SCRIPT_DIR}/installation.log
-  source ${SCRIPT_DIR}/BackEnd/env/bin/activate
-  pip3 install nodeenv==1.3.3 | tee -a ${SCRIPT_DIR}/installation.log
-  python3 -m nodeenv ${SCRIPT_DIR}/FrontEnd/node_env | tee -a ${SCRIPT_DIR}/installation.log
+if [[ ! -d "${SCRIPT_DIR}/FrontEnd/node_env" ]] || [[ "$1" == "-force" ]]; then
+  echo "node environment was not yet set up. Setting up now... (This only needs to happen once.)" | tee -a "${SCRIPT_DIR}"/installation.log
+  source "${SCRIPT_DIR}"/BackEnd/env/bin/activate
+  pip3 install nodeenv==1.3.3 | tee -a "${SCRIPT_DIR}"/installation.log
+  python3 -m nodeenv "${SCRIPT_DIR}"/FrontEnd/node_env | tee -a "${SCRIPT_DIR}"/installation.log
   deactivate
-  source ${SCRIPT_DIR}/FrontEnd/node_env/bin/activate
-  cd ${SCRIPT_DIR}/FrontEnd
-  npm install -g @angular/cli --silent | tee -a ${SCRIPT_DIR}/installation.log
-  npm install --silent | tee -a ${SCRIPT_DIR}/installation.log
+  source "${SCRIPT_DIR}"/FrontEnd/node_env/bin/activate
+  cd "${SCRIPT_DIR}"/FrontEnd || exit 1
+  npm install -g @angular/cli --silent | tee -a "${SCRIPT_DIR}"/installation.log
+  npm install --silent | tee -a "${SCRIPT_DIR}"/installation.log
   deactivate_node
-  cd -
+  cd - || exit 2
 fi
 
-if [[ ! -f "${SCRIPT_DIR}/FrontEnd/src/environments/versions.ts" ]] || [[ "$1" = "-force" ]]
-then
-  echo "No git release versions file. creating one now... (This only needs to happen once.)" | tee -a ${SCRIPT_DIR}/installation.log
-  source ${SCRIPT_DIR}/FrontEnd/node_env/bin/activate
-  cd ${SCRIPT_DIR}/FrontEnd/
-  npm run prebuild.prod | tee -a ${SCRIPT_DIR}/installation.log
-  cd -
+if [[ ! -f "${SCRIPT_DIR}/FrontEnd/src/environments/versions.ts" ]] || [[ "$1" == "-force" ]]; then
+  echo "No git release versions file. creating one now... (This only needs to happen once.)" | tee -a "${SCRIPT_DIR}"/installation.log
+  source "${SCRIPT_DIR}"/FrontEnd/node_env/bin/activate
+  cd "${SCRIPT_DIR}"/FrontEnd/ || exit 3
+  npm run prebuild.prod | tee -a "${SCRIPT_DIR}"/installation.log
+  cd - || exit 4
   deactivate_node
 fi
 
-if [[ ! -f "${SCRIPT_DIR}/BackEnd/app/configuration.json" ]]
-then
+if [[ ! -f "${SCRIPT_DIR}/BackEnd/app/configuration.json" ]]; then
   echo "No configuration file found. Copying example configuration file (which will work for local testing) to ${SCRIPT_DIR}/BackEnd/app/configuration.json. You should edit this file before deployment." | tee -a ${SCRIPT_DIR}/installation.log
-  cp ${SCRIPT_DIR}/BackEnd/app/example_config.json ${SCRIPT_DIR}/BackEnd/app/configuration.json
+  cp "${SCRIPT_DIR}"/BackEnd/app/example_config.json "${SCRIPT_DIR}"/BackEnd/app/configuration.json
 fi
 
-if [[ ! -f "${SCRIPT_DIR}/BackEnd/app/schema_data/generated" ]] || [[ "$1" = "-force" ]]
-then
-    echo "Schemas not found. Generating local cache of schema versions... (This only needs to happen once.)" | tee -a ${SCRIPT_DIR}/installation.log
-    source ${SCRIPT_DIR}/BackEnd/env/bin/activate
-    ${SCRIPT_DIR}/BackEnd/app/schema_loader.py 2>&1 | tee -a ${SCRIPT_DIR}/installation.log
-    deactivate
-    touch ${SCRIPT_DIR}/BackEnd/app/schema_data/generated
+if [[ ! -f "${SCRIPT_DIR}/BackEnd/app/schema_data/generated" ]] || [[ "$1" == "-force" ]]; then
+  echo "Schemas not found. Generating local cache of schema versions... (This only needs to happen once.)" | tee -a "${SCRIPT_DIR}"/installation.log
+  source "${SCRIPT_DIR}"/BackEnd/env/bin/activate
+  "${SCRIPT_DIR}"/BackEnd/app/schema_loader.py 2>&1 | tee -a "${SCRIPT_DIR}"/installation.log
+  deactivate
+  touch "${SCRIPT_DIR}"/BackEnd/app/schema_data/generated
 fi
