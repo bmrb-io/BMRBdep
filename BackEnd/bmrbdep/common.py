aec9a65a5858722d22908e29c5db76ff924ee98c
Jon Wedell <jonwedell@gmail.com>
2019-11-27 10:33:20 -0600

    Update where the schema search looks.

--- BackEnd/bmrbdep/common.py
+++ BackEnd/bmrbdep/common.py
@@ -24,12 +24,19 @@ residue_mappings = {'polypeptide(L)': {'P': 'PRO', 'G': 'GLY', 'A': 'ALA', 'R': 
 def get_schema(version: str, schema_format: str = "json") -> Union[dict, TextIO]:
     """ Return the schema from disk. """
 
+    # When running locally
+    schema_dir = os.path.join(root_dir, '..', 'schema', 'schema_data')
+    if not os.path.exists(schema_dir):
+        schema_dir = os.path.join(root_dir, '..', 'schema_data')
+        if not os.path.exists(schema_dir):
+            raise IOError("No schema directory found.")
+
     try:
         if schema_format == "json":
-            with open(os.path.join('/opt/wsgi/schema_data', version + '.json.zlib'), 'rb') as schema_file:
+            with open(os.path.join(root_dir, '..', 'schema', 'schema_data', version + '.json.zlib'), 'rb') as schema_file:
                 schema = json.loads(zlib.decompress(schema_file.read()).decode())
         elif schema_format == "xml":
-            return open(os.path.join('/opt/wsgi/schema_data', version + '.xml'), 'r')
+            return open(os.path.join(root_dir, '..', 'schema', 'schema_data', version + '.xml'), 'r')
         else:
             raise ServerError('Attempted to load invalid schema type.')
     except IOError:
