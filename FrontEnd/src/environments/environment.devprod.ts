e3d99aa20db767125c87cb25c10569a4308dcd6f
Jon Wedell <jon@jonwedell.com>
2019-12-11 14:26:45 -0600

    Fix devprod serverURL

--- FrontEnd/src/environments/environment.devprod.ts
+++ FrontEnd/src/environments/environment.devprod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  serverURL: 'https://webapi.bmrb.wisc.edu/devel/deposition',
+  serverURL: '/deposition',
   contactEmail: 'support@bmrb.zendesk.com',
   supportURL: 'https://bmrb.zendesk.com/api/v2/requests.json'
 };
