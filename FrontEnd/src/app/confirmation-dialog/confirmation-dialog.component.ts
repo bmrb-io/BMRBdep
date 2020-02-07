10471845e768e7ed907545ba17caac1f68d9caa5
Jon Wedell <jon@jonwedell.com>
2020-02-07 12:31:30 -0600

    Update the confirmation dialogue to allow using it to get a text input as well. Will use this later to get the new deposition nickname when cloning a deposition.

--- FrontEnd/src/app/confirmation-dialog/confirmation-dialog.component.ts
+++ FrontEnd/src/app/confirmation-dialog/confirmation-dialog.component.ts
@@ -4,14 +4,18 @@ import {MatDialogRef} from '@angular/material/dialog';
 @Component({
   selector: 'app-confirmation-dialog',
   templateUrl: 'confirmation-dialog.component.html',
+  styleUrls: ['./confirmation-dialog.component.scss']
 })
 export class ConfirmationDialogComponent {
   public confirmMessage: string;
   public proceedMessage: string;
   public cancelMessage: string;
+  public inputBoxText: string;
+  public name: string;
 
   constructor(public dialogRef: MatDialogRef<ConfirmationDialogComponent>) {
     this.proceedMessage = 'Proceed';
     this.cancelMessage = 'Cancel';
+    this.inputBoxText = null;
   }
 }
