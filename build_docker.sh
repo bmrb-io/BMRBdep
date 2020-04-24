bea81e2b65b4d46117f9770c5f3f3c7a3416c9f7
Jon Wedell <jon@jonwedell.com>
2020-04-24 10:40:53 -0500

    Updates to the build script

--- build_docker.sh
+++ build_docker.sh
@@ -6,8 +6,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 "${SCRIPT_DIR}"/install.sh
 
 echo "Removing existing local docker image"
-sudo docker stop bmrbdep
-sudo docker rm bmrbdep
+sudo docker stop bmrbig
+sudo docker rm bmrbig
 
 # We're going to make a special directory with just the docker build context
 mkdir -p ${SCRIPT_DIR}/docker_build/
@@ -16,14 +16,14 @@ cp -rp ${SCRIPT_DIR}/wsgi.conf ${SCRIPT_DIR}/BackEnd/bmrbdep ${SCRIPT_DIR}/BackE
 find ${SCRIPT_DIR}/docker_build -name __pycache__ -print0 | xargs -0 rm -rf
 
 if [[ $# -eq 0 ]]; then
-  if ! sudo docker build -f ${SCRIPT_DIR}/Dockerfile -t bmrbdep ${SCRIPT_DIR}/docker_build; then
+  if ! sudo docker build -f ${SCRIPT_DIR}/Dockerfile -t bmrbig ${SCRIPT_DIR}/docker_build; then
     echo "Docker build failed."
     exit 2
   fi
   echo "Running in development mode."
-  sudo docker run -d --name bmrbdep -p 4444:9001 --restart=always -v /opt/wsgi/depositions:/opt/wsgi/depositions -v ${SCRIPT_DIR}/BackEnd/bmrbdep/configuration.json:/opt/wsgi/bmrbdep/configuration.json bmrbdep
+  sudo docker run -d --name bmrbig -p 4444:9001 --restart=always -v /opt/wsgi/depositions:/opt/wsgi/depositions -v ${SCRIPT_DIR}/BackEnd/bmrbdep/configuration.json:/opt/wsgi/bmrbdep/configuration.json bmrbdep
 else
-  if ! sudo docker build -f ${SCRIPT_DIR}/Dockerfile --build-arg configfile="$1" -t bmrbdep ${SCRIPT_DIR}/docker_build; then
+  if ! sudo docker build -f ${SCRIPT_DIR}/Dockerfile -t bmrbig ${SCRIPT_DIR}/docker_build; then
     echo "Docker build failed."
     exit 3
   fi
