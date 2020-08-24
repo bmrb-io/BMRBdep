f4cd8f0c5388c6f68214a70b23a3fb7c9beab481
Jon Wedell <jonwedell@gmail.com>
2020-08-24 14:50:57 -0500

    Fix an errant tab

--- BackEnd/bmrbdep/helpers/sqlite.py
+++ BackEnd/bmrbdep/helpers/sqlite.py
@@ -64,10 +64,10 @@ VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
                     params['bmrb_id'], params['pdb_id'], params['publication_doi']]
         else:
             sql = """
-    INSERT OR REPLACE INTO entrylog (bmrbig_id, submission_date, release_date, title, contact_person1,
-    author_email, restart_id, author_email, bmrb_id, pdb_id, publication_doi)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
-        args = [params['bmrbig_id'], params['submission_date'], params['release_date'],
+INSERT OR REPLACE INTO entrylog (bmrbig_id, submission_date, release_date, title, contact_person1,
+author_email, restart_id, author_email, bmrb_id, pdb_id, publication_doi)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+            args = [params['bmrbig_id'], params['submission_date'], params['release_date'],
                 params['title'], params['contact_person1'], params['author_email'],
                 params['restart_id'], params['author_email'],
                 params['bmrb_id'], params['pdb_id'], params['publication_doi']]
