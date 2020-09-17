5c14b20da278e80aeca0e138639e1ec2e85d6d45
jon@jonwedell.com <f6ac4ceae0ad45089108fb1ed28d28b441a0866a>
2020-09-17 13:52:13 -0500

    Update the stats parser to have a separate option for fixing the issue with deposition_date.

--- BackEnd/bmrbdep/stats/__main__.py
+++ BackEnd/bmrbdep/stats/__main__.py
@@ -12,6 +12,8 @@ opt = optparse.OptionParser(usage="usage: %prog", version="1.0",
                             description="Update the entries in the Redis database.")
 opt.add_option("--file", action="store", dest="file_name", default='/tmp/entry_info.csv',
                help="Where to store the calculated information.")
+opt.add_option("--fix-deposition-date", action="store_true", dest="fix_deposition_date", default=False,
+               help="Fix a bug that existed in the deposition_date value in the submission_info.json file.")
 opt.add_option("--verbose", action="store_true", dest="verbose", default=False, help="Be verbose")
 # Parse the command line input
 (options, cmd_input) = opt.parse_args()
@@ -23,20 +25,25 @@ if options.verbose:
 else:
     logger.setLevel(logging.WARNING)
 
+
+if options.fix_deposition_date:
+    for entry in list_all_depositions():
+        with DepositionRepo(entry) as entry_repo:
+            if 'deposition_date' in entry_repo.metadata:
+                # Fix the records with a bug
+                if isinstance(entry_repo.metadata['deposition_date'], list):
+                    logging.warning("Fixing bug in deposition_date in submission_info file for entry %s" % entry)
+                    entry_repo.metadata['deposition_date'] = entry_repo.metadata['deposition_date'][0]
+                    entry_repo.commit('Fix the deposition date within the metadata to not be inside of a list.')
+
 output_file = open(options.file_name, 'w')
 output_csv = csv.writer(output_file)
 output_csv.writerow(['entry', 'deposition_time_minutes'])
 
 for entry in list_all_depositions():
     logging.info('Calculating stats for entry: %s' % entry)
-    with DepositionRepo(entry) as entry_repo:
+    with DepositionRepo(entry, read_only=True) as entry_repo:
         if 'deposition_date' in entry_repo.metadata:
-
-            # Fix the records with a bug
-            if isinstance(entry_repo.metadata['deposition_date'], list):
-                logging.warning("Fixing bug in deposition_date in submission_info file for entry %s" % entry)
-                entry_repo.metadata['deposition_date'] = entry_repo.metadata['deposition_date'][0]
-                entry_repo.commit('Fix the deposition date within the metadata to not be inside of a list.')
 
             # Get the time delta
             deposition_date_time = datetime.strptime(entry_repo.metadata['deposition_date'], "%I:%M %p on %B %d, %Y")
