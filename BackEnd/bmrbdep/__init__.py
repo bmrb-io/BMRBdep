d4175b3c5d7c1dc403f420bf702eef22dd1879a9
Jon Wedell <jon@jonwedell.com>
2025-01-22 17:26:17 -0600

    Bug fix.

--- BackEnd/bmrbdep/__init__.py
+++ BackEnd/bmrbdep/__init__.py
@@ -47,8 +47,8 @@ application.config.update(
     MAIL_PORT=configuration['smtp'].get('MAIL_PORT', 587),
     MAIL_USE_TLS=configuration['smtp'].get('MAIL_USE_TLS', True),
     MAIL_USE_SSL=configuration['smtp'].get('MAIL_USE_SSL', False),
-    MAIL_USERNAME=configuration['smtp'].config['MAIL_USERNAME'],
-    MAIL_PASSWORD=configuration['smtp'].config['MAIL_PASSWORD'],
+    MAIL_USERNAME=configuration['smtp']['MAIL_USERNAME'],
+    MAIL_PASSWORD=configuration['smtp']['MAIL_PASSWORD'],
 )
 mail = Mail(application)
 
