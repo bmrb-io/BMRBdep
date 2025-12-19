8b2aad28fee646dee087b881fec3ec5374d7eca8
Jon Wedell <jon@jonwedell.com>
2025-12-19 15:05:51 -0600

    Update the environment to support local/dev environment autodetection

--- FrontEnd/src/environments/environment.ts
+++ FrontEnd/src/environments/environment.ts
@@ -1,6 +1,12 @@
+
+let apiRoot = 'http://localhost:9000/deposition';
+if (window.location.hostname.startsWith('dev.')) {
+  apiRoot = '/deposition';
+}
+
 export const environment = {
   production: false,
-  serverURL: 'http://localhost:9000/deposition',
+  serverURL: apiRoot,
   contactEmail: 'help@bmrb.io',
   supportURL: 'https://nmrbox.zendesk.com/api/v2/requests.json'
 };
